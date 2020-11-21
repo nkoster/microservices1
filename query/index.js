06dae88545eaf913eef86c0b70393c26c4265e2e
Niels Koster <n.koster@portavita.eu>
2020-11-21 03:07:38 +0100

    Keep data in posts object

--- query/index.js
+++ query/index.js
@@ -3,21 +3,26 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const PORT = process.env.PORT || 4002
+const posts = {}
 
 app.use(bodyParser())
 app.use(cors())
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts)
 })
 
 app.post('/events', (req, res) => {
     const { type, data } = req.body
     if (type === 'PostCreated') {
+        const { id, title } = data
+        posts[id] = { id, title, comments: [] }
         console.log(type, data)
     }
 
     if (type === 'CommentCreated') {
+        const { id, content, postId } = data
+        posts[postId].comments.push({ id, content })
         console.log(type, data)
     }
 
