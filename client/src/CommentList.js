625c86efc75642e886e364b34c30a35717126166
Niels Koster <n.koster@portavita.eu>
2020-11-21 03:01:09 +0100

    Receive comments as a prop

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,18 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-const CommentList = ({ id }) => {
-
-    const [comments, setComments] = useState([])
-
-    const fetchComments = async _ => {
-        const response = await axios.get(`http://localhost:4001/posts/${id}/comments`)
-        setComments(response.data)
-    }
-
-    useEffect(_ => {
-        fetchComments()
-    }, [])
+const CommentList = ({ comments }) => {
 
     const renderedComments = comments.map(comment => {
         return (
