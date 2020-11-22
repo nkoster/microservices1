58884118600679012dd0ea24a6008ed5b0d46dc7
Niels Koster <n.koster@portavita.eu>
2020-11-22 02:42:24 +0100

    Store events and create endpoint to get all events

--- event-bus/index.js
+++ event-bus/index.js
@@ -2,18 +2,24 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     axios = require('axios'),
     app = express(),
-    port = process.env.PORT || 5000
+    port = process.env.PORT || 5000,
+    events = []
 
 app.use(bodyParser.json())
 
 app.post('/events', (req, res) => {
     const event = req.body
+    events.push(event)
     axios.post('http://localhost:4000/events', event)
     axios.post('http://localhost:4001/events', event)
     axios.post('http://localhost:4002/events', event)
     axios.post('http://localhost:4003/events', event)
     res.send({ status: 'OK' })
     console.log('event emitted')
+})
+
+app.get('/events', (req, res) => {
+    res.send(events)
 })
 
 app.listen(port, _ =>
