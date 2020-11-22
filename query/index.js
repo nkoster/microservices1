442d6ed772340fb3b7108830f9efb80d19d0038a
Niels Koster <n.koster@portavita.eu>
2020-11-22 02:43:48 +0100

    handelEvent() helper + get and process all events on startup

--- query/index.js
+++ query/index.js
@@ -1,9 +1,32 @@
 const express = require('express'),
     app = express(),
+    axios = require('axios'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     PORT = process.env.PORT || 4002,
-    posts = {}
+    posts = {},
+    handleEvent = (type, data) => {
+        if (type === 'PostCreated') {
+            const { id, title } = data
+            posts[id] = { id, title, comments: [] }
+            console.log(type, data)
+        }
+    
+        if (type === 'CommentCreated') {
+            const { id, content, postId, status } = data
+            posts[postId].comments.push({ id, content, status })
+            console.log(type, data)
+        }
+    
+        if (type === 'CommentUpdated') {
+            const { id, content, postId, status } = data
+            const post = posts[postId]
+            const comment = post.comments.find(comment => id === comment.id)
+            comment.status = status
+            comment.content = content
+            console.log(type, data)
+        }
+    }
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -14,30 +37,15 @@ app.get('/posts', (_, res) => {
 
 app.post('/events', (req, res) => {
     const { type, data } = req.body
-    if (type === 'PostCreated') {
-        const { id, title } = data
-        posts[id] = { id, title, comments: [] }
-        console.log(type, data)
-    }
-
-    if (type === 'CommentCreated') {
-        const { id, content, postId, status } = data
-        posts[postId].comments.push({ id, content, status })
-        console.log(type, data)
-    }
-
-    if (type === 'CommentUpdated') {
-        const { id, content, postId, status } = data
-        const post = posts[postId]
-        const comment = post.comments.find(comment => id === comment.id)
-        comment.status = status
-        comment.content = content
-        console.log(type, data)
-    }
-
+    handleEvent(type, data)
     res.send({})
 })
 
-app.listen(PORT, _ => 
+app.listen(PORT, async _ => {
     console.log(`Blog Query micro service running at ${PORT}`)
-)
+    const response = await axios.get('http://localhost:5000/events')
+    Object.values(response.data).forEach(event => {
+        console.log('Processing event', event.type)
+        handleEvent(event.type, event.data)
+    })
+})
