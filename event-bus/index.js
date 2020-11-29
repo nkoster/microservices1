cc765c080ea4fae5c9f7029b621f9a151a7da299
Niels Koster <n.koster@portavita.eu>
2020-11-30 00:38:06 +0100

    Update service URL + New version + Show new version

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.post('/events', (req, res) => {
     const event = req.body
     events.push(event)
-    axios.post('http://localhost:4000/events', event)
+    axios.post('http://nkoster-posts/events', event)
     axios.post('http://localhost:4001/events', event)
     axios.post('http://localhost:4002/events', event)
     axios.post('http://localhost:4003/events', event)
@@ -22,5 +22,7 @@ app.get('/events', (req, res) => {
     res.send(events)
 })
 
-app.listen(port, _ =>
-    console.log(`Event-Bus running on port ${port}`))
+app.listen(port, _ => {
+    console.log('v0.0.4')
+    console.log(`Event-Bus running on port ${port}`)
+})
