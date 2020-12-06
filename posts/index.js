09839567dc342506e0fa904a098bd0853e6fa2c3
Niels Koster <n.koster@portavita.eu>
2020-12-06 23:32:04 +0100

    Unique path

--- posts/index.js
+++ posts/index.js
@@ -10,11 +10,11 @@ const express = require('express'),
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
+// app.get('/posts', (req, res) => {
+//     res.send(posts)
+// })
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const { title } = req.body
     posts[id] = { id, title }
